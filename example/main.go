908f0709e74be1e4956edd22eede074b89d01c72
Cody Potter <cody.potter@gearbox.com>
2026-06-20 13:24:07 -0400

    example so i can see what I'm doing

---
+++ example/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/a-h/templ"
+	"github.com/codypotter/templ-calendar/calendar"
+)
+
+func main() {
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("example/assets"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now()
+		props := calendar.Props{
+			Year:  now.Year(),
+			Month: now.Month(),
+			Events: []calendar.Event{
+				{ID: "1", Title: "Team standup", Date: time.Date(now.Year(), now.Month(), 5, 9, 0, 0, 0, time.Local)},
+				{ID: "2", Title: "Sprint review", Date: time.Date(now.Year(), now.Month(), 15, 14, 0, 0, 0, time.Local)},
+			},
+		}
+		templ.Handler(indexPage(props)).ServeHTTP(w, r)
+	})
+
+	log.Println("listening on :8090")
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
